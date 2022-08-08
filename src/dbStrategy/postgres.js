0c28b7102ca17944bbaf9a88d9fb275adeec5458
EricChandia <eric.chandia.ti@gmail.com>
2022-08-08 13:37:17 -0300

    prepare for deploy

--- src/dbStrategy/postgres.js
+++ src/dbStrategy/postgres.js
@@ -2,16 +2,22 @@ import pkg from 'pg';
 
 const { Pool } = pkg;
 
-const connection = new Pool({
-  host: '127.0.0.1',
-  port: 5432,
-  user: 'postgres',
-  password: '210720',
-  database: 'shortly'
-});
-
 // const connection = new Pool({
-//   connectionString: process.env.DATABASE_URL,
+//   host: '127.0.0.1',
+//   port: 5432,
+//   user: 'postgres',
+//   password: '*****',
+//   database: 'shortly'
 // });
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(databaseConfig);
+
 
 export default connection;
